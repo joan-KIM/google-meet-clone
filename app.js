51049189e3a9e7b2575fe84e1dfd4f60e075689e
ethan-eunwu <ethan.eunwu@kakaocorp.com>
2020-08-27 00:40:21 +0900

    리펙토링

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
 import {Input} from './input.js';
 
-const $input = document.getElementById('input');
-const $applyBtn = document.getElementById('apply_button');
-
-Input($input, $applyBtn);
+Input();
--- input.js
+++ input.js
@@ -1,13 +1,24 @@
-function disabled(value) {
-  return value === '';
+function show($target) {
+  $target.style.display = 'block';
 }
 
-export function Input($target, $applyBtn){
-  $target.addEventListener('focus', () => {
-    $applyBtn.style.display = "block";
+export function Input(){
+  const $input = document.getElementById('input');
+  const $applyBtn = document.getElementById('apply_button');
+
+  function disabled() {
+    return $input.value === '';
+  }
+
+  $input.addEventListener('focus', () => {
+    show($applyBtn);
   });
 
-  $target.addEventListener('input', () => {
-    $applyBtn.disabled = disabled($target.value);
-  })
+  $input.addEventListener('input', () => {
+    $applyBtn.disabled = disabled();
+  });
+
+  $applyBtn.addEventListener('click', () => {
+    console.log('이동')
+  });
 }
